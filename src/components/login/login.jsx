49d6aeea379700bac1bda5fe6c8e6350ea061113
Hammam Elmuratdh <47317019+Hamam-Elmuratdh@users.noreply.github.com>
2020-01-13 17:55:00 +0100

    comments added

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -2,20 +2,20 @@ import React, { useState } from "react";
 import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input'
 import { Redirect } from 'react-router-dom';
-
+// using react hooks takes the user name and the password  
 export function Login(props) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [isLogged, setIslogged] = useState(false)
 
-
+//handling the submit of the password and the username 
     var handleSubmit = e => {
         e.preventDefault();
         let user = {
             username,
             password
         };
-
+// using fetch to send user information to the server
         fetch("https://nodes-chat-app.herokuapp.com/signin", {
             method: "POST",
             headers: {
@@ -23,6 +23,7 @@ export function Login(props) {
             },
             body: JSON.stringify(user)
         }).then((response) => response.json())
+        // saving the token and the refresh token to local storage
             .then((data) => {
                 console.log('Success:', data);
                 localStorage.setItem("username", username);
